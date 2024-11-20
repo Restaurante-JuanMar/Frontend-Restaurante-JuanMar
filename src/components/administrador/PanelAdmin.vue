a680b5957bae0e688daee114351e8d4788129607
Sebastian Sandoval <juansesdvsf@gmail.com>
2024-11-20 10:55:02 -0500

    Cambios 20/11

--- src/components/administrador/PanelAdmin.vue
+++ src/components/administrador/PanelAdmin.vue
@@ -2,11 +2,13 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import LogoJuanMar from '../../assets/juanmarlogo1.png';
-import Menu from '../../assets/Menú.jpg';
-import Reservacion from '../../assets/Reservacion.jpg';
-import Galeria from '../../assets/GaleriaFotos.jpg';
-import TrabajaNosotros from '../../assets/TrabajaConNosotros.jpg';
-import Contactenos from '../../assets/Contacenos.jpg';
+import Menu from '../../assets/LogosAdmin/carta_menu.png'
+import Reservacion from '../../assets/LogosAdmin/reservacion.png';
+import Galeria from '../../assets/LogosAdmin/galeria_fotos.png';
+import TrabajaNosotros from '../../assets/LogosAdmin/trabaja_con_nosotros.png';
+import Contactenos from '../../assets/LogosAdmin/contacenos_1.png';
+import Mision from '../../assets/LogosAdmin/granaje.png';
+import PlatoEspecial from '../../assets/LogosAdmin/plato_especial.png';
 
 const router = useRouter();
 
@@ -32,6 +34,14 @@ function irTrabajaConNosotros(){
 
 function irContactenosAdmin(){
     router.push('/contactenos-admin')
+}
+
+function irPlatoEspecialAdmin(){
+    router.push('/plato-especial')
+}
+
+function irMisionAdmin(){
+    router.push('/mision')
 }
 </script>
 
@@ -70,11 +80,20 @@ function irContactenosAdmin(){
                     <a @click="irContactenosAdmin()" style="cursor: pointer;"><img :src="Contactenos" class="img-fluid rounded-4" style="border-style: solid; border-color: #734a4a;" alt="Restaurante JuanMar"></a>
                 </div>
             </div>
+            <div class="col-lg-4 col-md-6 col-sm-3 mt-3 mb-3">
+                <div class="card card-degradado rounded-4">
+                    <a @click="irMisionAdmin()" style="cursor: pointer;"><img :src="Mision" class="img-fluid rounded-4" style="border-style: solid; border-color: #734a4a;" alt="Restaurante JuanMar"></a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-3 mt-3 mb-3">
+                <div class="card card-degradado rounded-4">
+                    <a @click="irPlatoEspecialAdmin()" style="cursor: pointer;"><img :src="PlatoEspecial" class="img-fluid rounded-4" style="border-style: solid; border-color: #734a4a;" alt="Restaurante JuanMar"></a>
+                </div>
+            </div>
         </div>
     </div>
     </div>
 </template>
 
 <style scoped>
-
 </style>
